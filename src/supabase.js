e2851520fba36b419096240ed2eb62e1ea6397c1
amrkv526 <93342376+amrkv526@users.noreply.github.com>
2023-11-20 13:43:56 -0500

    Revert "hidden keys"
    This reverts commit 4892d6bc4f4c63047c6c1f8465527e41bc517d58.

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.supabaseUrl;
-const supabaseKey = process.env.supabaseKey;
+const supabaseUrl = "https://swccdmvjckwltkyvodvn.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3Y2NkbXZqY2t3bHRreXZvZHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzk1MTUsImV4cCI6MTk5Mzk1NTUxNX0.HITXhwIic0UlKzEHiUJWmGZnZmIqiZ86lTRm2BR_5ck";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
